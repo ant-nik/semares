e7eb0039464d8942e2772ca57e2106c8a38e044e
Anton Nikonov <ant.nik.nik@mail.ru>
2024-04-08 13:26:32 +0300

    adds basic files for stereo camera simulation in Webots

---
+++ README.md
@@ -0,0 +1,3 @@
+# Semares bot
+Implementation of a robot's algorithms for semantic map understanding and automatic reasoning by human-friendly knowledge description.
+More details can be found at https://github.com/ant-nik/semares.
---
+++ sim/stereo-camera/controllers/python_code/python_code.py
@@ -0,0 +1,27 @@
+""" 
+https://docs.opencv.org/4.x/d9/db7/tutorial_py_table_of_contents_calib3d.html
+"""
+from controller import Robot
+import cv2
+
+robot = Robot()
+
+timestep = 1000 # int(robot.getBasicTimeStep())
+
+rcamera = robot.getDevice("right camera")
+lcamera = robot.getDevice("left camera")
+
+rcamera.enable(timestep)
+lcamera.enable(timestep)
+
+i = 0
+
+while (robot.step(timestep) != -1):
+  # Read the sensors, like:
+  rcamera.saveImage(filename=f"image{i}_r.jpg", quality=50)
+  lcamera.saveImage(filename=f"image{i}_l.jpg", quality=50)
+  i = i + 1
+  pass
+  
+
+# Enter here exit cleanup code
